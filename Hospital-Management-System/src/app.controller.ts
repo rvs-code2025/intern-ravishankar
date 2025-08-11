612b4a357c923c9755b8c9493ac9f53608f5f481
civilstudy2024@gmail.com <civilstudy2024@gmail.com>
2025-08-11 13:00:35 +0530

    Render Testing

--- Hospital-Management-System/src/app.controller.ts
+++ Hospital-Management-System/src/app.controller.ts
@@ -4,9 +4,8 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Nest.js App is running on Render';
   }
 }
